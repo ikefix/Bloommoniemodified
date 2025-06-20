3de47e491b23600d1d0031c93215187c0998b595
ikefix <omekeikechukwu877@gmail.com>
2025-06-20 02:57:22 +0100

    hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         
-    @vite([ 'resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
 
         {{-- <!-- Styles -->
         <style>

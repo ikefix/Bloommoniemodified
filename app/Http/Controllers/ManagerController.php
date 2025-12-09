3ed9014ef397ac4566eabb0a89e089ab54eba0e1
ikefix <omekeikechukwu877@gmail.com>
2025-12-09 15:40:50 +0100

    adjustments 0.1

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -172,11 +172,8 @@ class ManagerController extends Controller
                 $query->where('name', 'like', "%$search%");
             })
             ->paginate(10);
-        
-        // Fetch shops
-        $shops = Shop::all();  // Assuming the manager has access to all shops, adjust if needed
 
-        return view('manager.product', compact('products', 'categories', 'search', 'shops'));
+        return view('manager.product', compact('products', 'categories', 'search'));
     }
     
     
